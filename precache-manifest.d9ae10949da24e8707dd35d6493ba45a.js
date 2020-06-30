8820614a3dac1bca89ed529fe2d03b13b7b24e95
Rato2392 <joao.r.rato@gmail.com>
2020-06-30 09:24:54 +0100

    Updates

---
+++ precache-manifest.d9ae10949da24e8707dd35d6493ba45a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd36c92747e0d2913994df9eb3701e7",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "7a4330fdad182b3b6bdb",
+    "url": "/react_app/static/css/main.7225289e.chunk.css"
+  },
+  {
+    "revision": "4ae1ad529ebcb62da147",
+    "url": "/react_app/static/js/2.36d50bf9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_app/static/js/2.36d50bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a4330fdad182b3b6bdb",
+    "url": "/react_app/static/js/main.738ed61c.chunk.js"
+  },
+  {
+    "revision": "b923d741eeba4bf347a1",
+    "url": "/react_app/static/js/runtime-main.80b4d2f4.js"
+  }
+]);
